5000bfe99314405cb39c105c20dd05b1716f4c7e
EvilSmurf06 <79266119+EvilSmurf06@users.noreply.github.com>
2021-06-11 14:59:21 +0300

    Update Card.js

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,11 +1,7 @@
 import React, {Component } from 'react';
-// import Jumbotron from 'react-bootstrap/Jumbotron';
 import HomePage from './HomePage.js'
 
 class Card extends Component{
-  constructor(props){
-    super(props)
-  }
   render(){
     return(
       <>
